8263a087998b476da5f62bf7b503a4950632f83a
six <mtangrad5@gmail.com>
2026-04-21 05:01:07 -0400

    tracing / logging
    took distributed key-value store from libp2p examples

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@ pub(crate) mod config;
 
 use crate::config::LcConfig;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use tracing::info;
+//use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = tracing_subscriber::fmt()
+        .pretty()
+        .compact()
+        .with_ansi(true)
+        .with_target(true)
+        // .with_env_filter(EnvFilter::from_default_env())
+        // .with_thread_ids(true)
+        .try_init();
+
+    info!("Started lc");
+
     let cfg: LcConfig = confy::load("lc", Some("lc"))?;
-    dbg!(&cfg);
+    // dbg!(&cfg);
+    info!("Started with these settings {:#?}", &cfg);
 
     Ok(())
 }
