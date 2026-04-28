c6d527ce59910f12cfd791d2ba97f3735d887cd4
six <mtangradi5@gmail.com>
2026-04-28 13:43:29 -0400

    config update to hold key location

--- src/config/libp2p_config.rs
+++ src/config/libp2p_config.rs
@@ -1,12 +1,19 @@
 use serde::{Deserialize, Serialize};
 
+// / Keys will be absolute path, defaulting to the config directory
+// /
+// /
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Libp2pConfig {
     is_server: bool,
+    key_location: String,
 }
 
 impl Default for Libp2pConfig {
     fn default() -> Self {
-        Self { is_server: true }
+        Self {
+            is_server: true,
+            key_location: "~/.config/lc".into(),
+        }
     }
 }
