d40d94bab0da91f20291e97d9ee5c135ddc2a671
six <mtangradi5@gmail.com>
2026-04-28 13:43:29 -0400

    basic ping to test connectivity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 pub(crate) mod config;
 mod key;
 
+use std::{time::Duration, u64};
+
 use crate::config::LcConfig;
 
+use multiaddr::multiaddr;
+
+use futures::StreamExt;
+use libp2p::{multiaddr, noise, ping, swarm::SwarmEvent, tcp, yamux, Multiaddr};
 use tracing::info;
 //use tracing_subscriber::EnvFilter;
 
@@ -29,7 +35,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let keypair = key::get_key(key_path);
 
-    dbg!(&keypair);
+    let location_addr = multiaddr!(Ip4([127, 0, 0, 1]), Tcp(10500u16));
 
-    Ok(())
+    dbg!(&location_addr);
+
+    let mut swarm = libp2p::SwarmBuilder::with_existing_identity(keypair)
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
+        .build();
+
+    swarm.listen_on(location_addr)?;
+
+    if let Some(addr_remote) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr_remote.parse()?;
+        let _ = swarm.dial(remote);
+        println!("dialed {addr_remote}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+
+    // Ok(())
 }
